deb5ee41eb2e7f378e094fac6ddfa94dd84dc24b
Helge Skogly Holm <helge.holm@gmail.com>
2015-08-19 17:58:50 +0200

    Fixed first moveandscale test, and added a new one.

--- test/scale.spec.js
+++ test/scale.spec.js
@@ -9,7 +9,17 @@ describe("move and scale", function() {
   it("changes nothing if references are unchanged", function() {
     var input = fs.readFileSync("test/data/simple.vtt").toString();
     var data = read(input);
-    adjust.move(data.cues, data.cues[0], data.cues[1]);
+    adjust.moveAndScale(data.cues, data.cues[0], data.cues[1]);
     assert.equal(write(data), input);
+  });
+
+  it("can scale and not move", function() {
+    var input = fs.readFileSync("test/data/simpler.vtt").toString();
+    var output = fs.readFileSync("test/data/simpler-scaleup.vtt").toString();
+    var data = read(input);
+    var cue3 = JSON.parse(JSON.stringify(data.cues[2]));
+    cue3.start += 20000;
+    adjust.moveAndScale(data.cues, data.cues[0], cue3);
+    assert.equal(write(data), output);
   });
 });
