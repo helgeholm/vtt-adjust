1099614eb5acaf63defd7c44cf577244cfa60c8f
Helge Skogly Holm <helge.holm@gmail.com>
2015-08-20 16:10:18 +0200

    Fixed an issue with randomized scale test.

--- test/randomized.spec.js
+++ test/randomized.spec.js
@@ -47,12 +47,19 @@ describe("randomized move", function() {
     for (var i=0; i < iterations; i++) {
       var refCues = randy.sample(range(origCues.length), 2);
       var scaleDist = randy.randInt(1, 10000);
-      var moveCue = origCues[refCues[0]];
+      var moveCue = cp(origCues[refCues[0]]);
       var scaleCue = cp(origCues[refCues[1]]);
       scaleCue.start += scaleDist;
       adjust.moveAndScale(data.cues, moveCue, scaleCue);
+
+      // Truncate any timestamps that we've moved below 0, since that's
+      // expected to happen, and they're outside the test scope.
+      data.cues.forEach(function(cue) {
+        if (cue.start < 0) cue.start = 0;
+        if (cue.end < 0) cue.end = 0;
+      });
+
       var data2 = read(write(data));
-      console.log(refCues);
       assert.equal(data2.cues[refCues[0]].start, origCues[refCues[0]].start);
       assert.equal(data2.cues[refCues[0]].end, origCues[refCues[0]].end);
       assert.equal(data2.cues[refCues[1]].start,
