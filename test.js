112f98fd579f889af8a1a8234d13a491fffe1eec
Helge Skogly Holm <helge.holm@gmail.com>
2015-08-20 18:55:15 +0200

    Manual test code no longer needed.

--- test.js
+++
@@ -1,8 +0,0 @@
-var read = require('./readSegments');
-var write = require('./writeSegments');
-var vtt = require('fs').readFileSync('test/data/elephant.vtt').toString();
-
-var data = read(vtt);
-var vtt2 = write(data);
-
-console.log(vtt2);
