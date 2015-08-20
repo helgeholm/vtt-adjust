f6f6b97cfcea153b41f1e7badac40404c2452cb0
Helge Skogly Holm <helge.holm@gmail.com>
2015-08-20 14:54:20 +0200

    Added randomized move test.

---
+++ test/randomized.spec.js
@@ -0,0 +1,36 @@
+var adjust = require('../adjust');
+var read = require('../readSegments');
+var write = require('../writeSegments');
+
+var fs = require('fs');
+var assert = require('assert');
+var randy = require('randy');
+
+// Determinism!
+randy.setState({"seed":[1086990994,1028312256,-269792373,-1989843090,-528923669,-589875010,1470268014,132703380,-615785460,-767938092,-209901597,358900416,-104513501,-242605169,75406930,1517592762,-8173077,1678948693,-1666915941,-9491474,-2048472118,1336790548,-270557609,-57483379,-1604043572,1313548012,170064268,1539635231,709334312,-1372375320,1965458340,-1049887153],"idx":1});
+
+var iterations = 1000;
+
+function cp(obj) { return JSON.parse(JSON.stringify(obj)); }
+
+describe("randomized move", function() {
+  var input = fs.readFileSync("test/data/simple.vtt").toString();
+
+  it("can move a random cue a random amount", function() {
+    this.slow(500);
+    for (var i=0; i < iterations; i++) {
+      var cueIdx = randy.randInt(3);
+      var moveDist = randy.randInt(1, 10000);
+      var data = read(input);
+      var origCues = data.cues.map(cp);
+      var movedCue = cp(origCues[cueIdx]);
+      movedCue.start += moveDist;
+      adjust.move(data.cues, movedCue);
+      var data2 = read(write(data));
+      for (var j=0; j < data2.cues.length; j++) {
+        assert(data2.cues[j].start == origCues[j].start + moveDist);
+        assert(data2.cues[j].end == origCues[j].end + moveDist);
+      }
+    }
+  });
+});
