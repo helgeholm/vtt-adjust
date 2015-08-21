9da47c8129141e9d7283c778a99a81a0d5895ff3
Helge Skogly Holm <helge.holm@gmail.com>
2015-08-21 15:26:53 +0200

    Removed concept of spent adjuster object.

--- index.js
+++ index.js
@@ -6,30 +6,27 @@ function cp(obj) { return JSON.parse(JSON.stringify(obj)); }
 
 module.exports = function readString(vtt) {
   var data = read(vtt);
-  var visibleCues = data.cues.map(function(cue, idx) {
-    return {
-      id: idx,
-      start: cue.start,
-      text: cue.rest.join('\n')
-    };
-  });
 
-  function setObjectSpent() {
-    adjuster.move = spentPlaceholder;
-    adjuster.moveAndScale = spentPlaceholder;
-  }
+  // Representation of cues that will be exposed via API.
+  var visibleCues = calculateVisibleCues();
 
-  function spentPlaceholder() {
-    throw new Error('Adjustment already applied.');
+  function calculateVisibleCues() {
+    return data.cues.map(function(cue, idx) {
+      return {
+        id: idx,
+        start: cue.start,
+        text: cue.rest.join('\n')
+      };
+    });
   }
 
   function move(cueId, newStartMs) {
     if (typeof cueId == 'object')
       cueId = cueId.id;
-    setObjectSpent();
     var anchor = cp(data.cues[cueId]);
     anchor.start = newStartMs;
     adjust.move(data.cues, anchor);
+    visibleCues = calculateVisibleCues();
   }
   
   function moveAndScale(cueId1, newStartMs1, cueId2, newStartMs2) {
@@ -37,12 +34,12 @@ module.exports = function readString(vtt) {
       cueId1 = cueId1.id;
     if (typeof cueId2 == 'object')
       cueId2 = cueId2.id;
-    setObjectSpent();
     var anchor = cp(data.cues[cueId1]);
     anchor.start = newStartMs1;
     var scaleRef = cp(data.cues[cueId2]);
     scaleRef.start = newStartMs2;
     adjust.moveAndScale(data.cues, anchor, scaleRef);
+    visibleCues = calculateVisibleCues();
   }
 
   function toString() {
